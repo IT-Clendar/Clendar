a0ff35f6bf93015615faf2b6c65d607005ab5b4d
HS <heesealee@gmail.com>
2025-06-07 19:20:30 +0900

    Update __init__.py after Firebase key cleanup

--- model/__init__.py
+++ model/__init__.py
@@ -1,1 +1,11 @@
 from .recommender import recommend_for_user
+import os
+import json
+import firebase_admin
+from firebase_admin import credentials
+
+if not firebase_admin._apps:
+    firebase_json = os.environ.get("FIREBASE_CREDENTIALS")
+    if firebase_json:
+        cred = credentials.Certificate(json.loads(firebase_json))
+        firebase_admin.initialize_app(cred)
